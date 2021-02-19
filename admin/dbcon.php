dcb9e4c4a9031b23dfcd3df378977142ee728234
christopher mignon <dspmosfet@gmail.com>
2021-02-20 02:09:35 +0800

    inital commit

---
+++ admin/dbcon.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect('localhost','root','','Conceptionlmsdb') or die(mysqli_error());
+?>
---
+++ title_index.php
@@ -0,0 +1,42 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/logg.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+								<h3>
+
+								<p>Conception</p>
+								</h3>
+							<p class="chmsc">An initiative to introduce an Online Learning platform to secondary schools in Grenada</p>
+							
+
+							
+						
+								
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												
+												<p> Online Learning Solution </p>
+												<p>Bringing learning to your fingertips</p>
+								</div>		
+						</div>		
+				</div>
